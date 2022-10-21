d3a5fea92961ba897cdb6b9eb7ce4017464deae5
Matthew <mattj20@gmail.com>
2022-10-21 16:17:58 -0700

    commit tree 68d5bae77d4d45145e2b513f5eb0a0a984f82239

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(something = "*") {
+    return function(param = "special") {
+        return `You are ${something + param + something}!`
+    }
+}
+
+const Calculator = {
+    add: function(num1, num2) {
+        return num1 + num2
+    },
+    subtract: function(num1, num2) {
+        return num1 - num2
+    },
+    multiply: function(num1, num2) {
+        return num1 * num2
+    },
+    divide: function(num1, num2) {
+        return num1 / num2
+    }
+}
+
+function actionApplyer(num, arrFunctions) {
+    if (arrFunctions.length === 0) {
+    return num
+    }
+    else {
+        let test = num
+        for (let i=0; i < arrFunctions.length; i++) {
+            test = arrFunctions[i](test)
+        }
+        return test
+    }
+}
